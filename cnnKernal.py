99fb99bbf4500038fb84520878b69b3b2fb3bcf5
Wesmo123 <wlwesley464@gmail.com>
2024-04-20 21:26:09 +0200

    Comments and File Deletion
    - Commented the convolve method in the cnnKernal class
    
    - deleted 2 redundant files pooling and ReLU

--- cnnKernal.py
+++ cnnKernal.py
@@ -41,21 +41,18 @@ class cnnKernal:    #class for the cnn's kernals, allows easy access to an kerna
 
     def convolve(self, inputMap, mapdim1, mapdim2): # convolution operation, HAS NO STRIDING OR PADDING OPTIONS
 
-        container = np.zeros((1, mapdim2 - (self.size - 1))) # container for outputmap, will NOT work with striding (3d, rows , coloumns)
+        container = np.zeros((1, mapdim2 - (self.size - 1))) # container for outputmap, uses a array of zeros to keep its shape
         for steps in range(0, mapdim1 - (self.size - 1)): # calculates the number of rows in the output map and uses them to tell the kernal when to "step down" to the next layer, WILL NOT WORK WITH STRIDING
-            rowcontainer = np.array([])
+            rowcontainer = np.array([]) # container for the convolved rows before they are added to the final feature map array
             for coloumnL in range(0, mapdim2 - (self.size - 1)):    # tracks the back end of the kernal to know when to stop and step down
                 if self.depth != 1:
                     convolve = 0
-                    for iter in range(0, self.depth):
-                        convolve = convolve + np.sum(inputMap[iter][0 + steps : self.size + steps, 0 + coloumnL : self.size + coloumnL] * self.kArray[iter])
-                    convolve = convolve + self.bias[self.kernalNo]
-                    rowcontainer = np.append(rowcontainer, convolve)
+                    for iter in range(0, self.depth):   #performs the convolution operation for however many layers there are
+                        convolve = convolve + np.sum(inputMap[iter][0 + steps : self.size + steps, 0 + coloumnL : self.size + coloumnL] * self.kArray[iter])    #the convolution operation done by elementwise multiplication
+                    convolve = convolve + self.bias[self.kernalNo]  # adds bias into the convolution
+                    rowcontainer = np.append(rowcontainer, convolve) # appends the convolved element to the new row
 
-            container = np.vstack((container, rowcontainer))
+            container = np.vstack((container, rowcontainer)) # adds the new row onto the container array, which is our new featuremap
 
-        container = np.delete(container, (0), axis=0)
+        container = np.delete(container, (0), axis=0)   # deletes the placeholder zero array used to keep the containers shape
         return container
-
-
-        
